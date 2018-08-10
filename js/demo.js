48487420ad2e581e372aa9dfc17fafea6590a0ea
Ronald Joseph <rex.rony@gmail.com>
2018-08-11 00:30:27 +0500

    First

---
+++ js/demo.js
@@ -0,0 +1,20 @@
+/**
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+{
+	setTimeout(() => document.body.classList.add('render'), 10);
+	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+	const total = navdemos.length;
+	const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+	const navigate = (linkEl) => {
+		document.body.classList.remove('render');
+		document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+	};
+}
